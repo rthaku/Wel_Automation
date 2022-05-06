f754adf617a79e4ca880e118b464f12c5dbe8f6c
rthakur <rthakur@wiley.com>
2022-05-06 15:32:37 -0400

    FirstPlayPr

---
+++ pages/POManager.js
@@ -0,0 +1,17 @@
+//const {LoginPage}= require('./LoginPage');
+
+//class POManager
+//{
+//constructor(page)
+//{
+
+//this.page=page;
+//this.loginPage = new LoginPage(this.page);
+//}
+
+//getLoginPage()
+ //   {
+  //      return this.loginPage;
+  //  }
+///}
+//module.exports={POManager};
