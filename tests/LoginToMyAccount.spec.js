8dc13c8a6e10e486a8e5d08200b232d72efb7372
rthakur <rthakur@wiley.com>
2022-05-09 20:26:02 -0400

    changes

--- tests/LoginToMyAccount.spec.js
+++ tests/LoginToMyAccount.spec.js
@@ -2,9 +2,8 @@ const { test,expect } = require("@playwright/test");
 const{loginPage, LoginPage}= require('../pages/LoginPage');
 
 test('Login to MyAccountPage', async({page})=> {
-
  const loginPage= new LoginPage(page);
- const username ="rthakur@wiley.com";
+ const username ="rajshr.72+alm2@gmail.com";
  const password ="Password@1234";
  
 await loginPage.loginToMyAccount(username,password);
