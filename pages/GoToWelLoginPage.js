0f610df7512d0dcce00f017ad893a6670cf34571
rthakur <rthakur@wiley.com>
2022-06-09 10:34:32 -0400

    addchanges

--- pages/GoToWelLoginPage.js
+++ pages/GoToWelLoginPage.js
@@ -18,4 +18,4 @@ class GoToWelLoginPage {
         }
 
    }      
-module.exports= {GoToWelLoginPage};
+module.exports= {GoToWelLoginPage}
---
+++ pages/Wel_Tools/ConsumerPage.js
@@ -0,0 +1,50 @@
+const { expect } = require("@playwright/test");
+
+class ConsumerPage {
+
+    constructor(page)
+{
+    this.page = page;
+    this.consumer = page.locator('text=CONSUMERS');
+    this.home = page.locator('text=Home');
+    this.logout = page.locator('text=Logout');
+    this.plusConsumerBtn = page.locator('text=+');
+    this.closeBtn = page.locator('text=Close');
+    this.footer = page.locator('fixed-table-pagination')
+    
+}
+
+async clickOnConsumerBtn()
+{
+    await this.consumer.click();
+    await expect(page).toHaveURL('https://services-internal.non-prod.efficientlearning.com/wel-tools/consumers');
+   
+} 
+
+async clickOnPlusConsumerBtn()
+{
+    await expect(this.plusConsumerBtn).toBeVisible();
+    await this.clickOnConsumerBtn.click();
+    await expect(this.closeBtn).toBeVisible();
+    await this.closeBtn.click();
+    await expect(page).toHaveURL('https://services-internal.non-prod.efficientlearning.com/wel-tools/consumers');
+}  
+
+async logOut()
+{
+    await expect(this.logout).toBeVisible();
+    await page.locator('text=Logout').click();
+    await expect(page).toHaveURL('https://services-internal.non-prod.efficientlearning.com/wel-tools/login?logout')
+
+}
+
+async footerDetails()
+{
+
+
+}
+
+
+
+}
+module.exports= {ConsumerPage};
